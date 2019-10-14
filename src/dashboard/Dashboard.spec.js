584a48a45ea294a2f1e0e8c8fa42d815f2eceddd
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-14 13:24:38 +0100

    set up project and check sample tests run

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
-// Test away
+
+import Dashboard from './Dashboard';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />);
+});
+
+describe("display component", () => {
+  test("renders", () => {
+    expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+  });
+});
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from './Display';
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = rtl.render(<Display />);
+});
+
+describe("display component", () => {
+  test("renders", () => {
+    expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+  });
+});
