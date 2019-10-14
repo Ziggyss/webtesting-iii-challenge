0862ece472af558fd7ea9bba154d6b76eb7902fc
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-10-14 13:39:19 +0100

    test gate and dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -13,8 +13,23 @@ beforeEach(() => {
 });
 
 describe("display component", () => {
-  test("renders", () => {
+  test("shows controls and initial display", () => {
     expect(wrapper.queryByText(/locked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/lock gate/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/close gate/i)).toBeInTheDocument();
   });
+
+  test("shows default values - unlocked and open", () => {
+    expect(wrapper.queryByText(/unlocked/i)).toBeInTheDocument();
+    expect(wrapper.queryByText(/open/i)).toBeInTheDocument();
+  })
+
+  test("cannot be closed or opened if locked", () => {
+    rtl.fireEvent.click(wrapper.queryByText(/lock gate/i));
+    expect(wrapper.queryByText(/open gate/i)).not.toBeInTheDocument;
+    expect(wrapper.queryByText(/close gate/i)).not.toBeInTheDocument;  
+  })
 });
+
 
